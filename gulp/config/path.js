61c31f25a62985d3898878252177627a1cf69da6
John Doe <montekke@gmail.com>
2022-10-19 12:21:04 +0300

    fix 1.0.1

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,36 +6,36 @@ const buildFolder = './dist';
 const srcFolder = './src';
 
 export const path = {
-	build: {
-		js: `${buildFolder}/js/`,
-		css: `${buildFolder}/css/`,
-		html: `${buildFolder}/`,
-		images: `${buildFolder}/img/`,
-		fonts: `${buildFolder}/fonts/`,
-		files: `${buildFolder}/files/`,
-		mailer: `${buildFolder}/mailer/`,
-	},
-	src: {
-		js: `${srcFolder}/js/app.js`,
-		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
-		svg: `${srcFolder}/img/**/*.svg`,
-		scss: `${srcFolder}/scss/style.scss`,
-		html: `${srcFolder}/*.html`, //	html: `${srcFolder}/*.pug`,
-		files: `${srcFolder}/files/**/*.*`,
-		mailer: `${srcFolder}/mailer/**/*.*`,
-		svgicons: `${srcFolder}/svgicons/*.svg`,
+  build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    images: `${buildFolder}/img/`,
+    fonts: `${buildFolder}/fonts/`,
+    files: `${buildFolder}/files/`,
+    mailer: `${buildFolder}/mailer/`,
+  },
+  src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`, //	html: `${srcFolder}/*.pug`,
+    files: `${srcFolder}/files/**/*.*`,
+    mailer: `${srcFolder}/mailer/**/*.*`,
+    svgicons: `${srcFolder}/svgicons/*.svg`,
 
-	},
-	watch: {
-		js: `${srcFolder}/js/**/*.js`,
-		scss: `${srcFolder}/scss/**/*.scss`,
-		html: `${srcFolder}/**/*.html`, // html: `${srcFolder}/**/*.pug`,
-		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,ico,gif,webp}`,
-		files: `${srcFolder}/files/**/*.*`,
-	},
-	clean: buildFolder,
-	buildFolder: buildFolder,
-	srcFolder: srcFolder,
-	rootFolder: rootFolder,
-	ftp: `nasuchan.ru/htdocs/www/`
+  },
+  watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`, // html: `${srcFolder}/**/*.pug`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,ico,gif,webp}`,
+    files: `${srcFolder}/files/**/*.*`,
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  ftp: `nasuchan.ru/htdocs/www/`
 }
